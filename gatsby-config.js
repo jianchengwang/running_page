554e4fb709822b58280e8c37814441ad6d729176
jianchengwang <jiancheng_wang@yahoo.com>
2023-03-02 23:25:39 +0800

    add gpx

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,18 +1,19 @@
 module.exports = {
-  pathPrefix: process.env.PATH_PREFIX || '/',
+  // pathPrefix: process.env.PATH_PREFIX || '/',
+  pathPrefix: '/running_page',
   siteMetadata: {
     siteTitle: 'Running Page',
-    siteUrl: 'https://yihong.run',
-    logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
+    siteUrl: 'https://jianchengwang.github.io/running_page',
+    logo: 'https://blog.res.jianchengwang.info/avatar.jpg',
     description: 'Personal site and blog',
     navLinks: [
       {
         name: 'Blog',
-        url: 'https://yihong.run/running',
+        url: 'http://jianchengwang.info',
       },
       {
         name: 'About',
-        url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
+        url: 'http://jianchengwang.info/about',
       },
     ],
   },
